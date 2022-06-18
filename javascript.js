7fbea3d8e4c04e58a8efe6bb41ec29395c5ebd97
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-06-18 19:31:31 +0530

    code added

--- javascript.js
+++ javascript.js
@@ -35,3 +35,45 @@ console.log(myArray);
 
 let nameArray = ["Aswin", "Satheesh"]
 console.log(nameArray[1]);
+
+let n = "aswin";
+console.log(n.length);
+
+console.log(n.indexOf('w'));
+
+let b = "name";
+let z = b.slice(0, 3); //slice function
+console.log(z);
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+let arr = ["Aswin", "Kannan", "Kuttu"];
+
+for (let i = 0; i < arr.length; i++) {
+    document.write(arr[i] + "<br>");
+
+}
+
+
+for (let x of arr) { //  for of loop
+    document.write(x + " ");
+}
+
+let myobj = {
+    firstN: "Anu",
+    lastN: " Raj",
+    age: 23
+};
+let x = 0;
+for (x in myobj) { //for in loop 
+    document.write(myobj[x] + " ");
+}
+
+
+function alertFun() {
+    alert("You have clicked this button!");
+}
