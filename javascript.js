4dcf3f5a8ed4e5f3ae6698e55d4d01a7fc088f2d
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-06-18 18:20:58 +0530

    javascript is getting more intresting

---
+++ javascript.js
@@ -0,0 +1,37 @@
+function myFun() {
+    var a = 10;
+    // console.log(a);
+    return a;
+}
+
+
+var newValue = myFun();
+var total = null;
+
+total = newValue + 100;
+
+console.log(total);
+
+// alert("Blocked"); // to create alert box
+
+
+let user = {
+    firstName: "Aswin",
+    lastName: "Satheesh"
+};
+
+
+console.log(user.lastName);
+console.log(user.firstName);
+console.log(user);
+
+
+//Array
+
+
+let myArray = [1, 2, 3, 4, 5]
+
+console.log(myArray);
+
+let nameArray = ["Aswin", "Satheesh"]
+console.log(nameArray[1]);
